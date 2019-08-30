481e50f5379a62ceb91ed1b1ffa3820a1f28bc9e
Alex <gnidkoalex@gmail.com>
2019-08-30 16:34:49 +0300

    edit works

--- controllers/products-controller.js
+++ controllers/products-controller.js
@@ -30,12 +30,22 @@ class ProductsController{
         })
     }
 
-    static updateProduct(pic,data){
+    static updateProductWithPic(pic,data){
         return new Promise((resolve,reject)=>{
             let staticUrl= `http://localhost:2200/pics/${pic}`
             console.log("im data")
             console.log(data)
             productsModel.findOneAndUpdate({_id:data.id},{image:staticUrl,productName:data.productName,price:data.price,categoryId:data.categoryId},(err,results)=>{
+                if(err) reject(err);
+                resolve(results)
+            })
+        })
+    }
+    static updateProductWithoutPic(data){
+        return new Promise((resolve,reject)=>{
+            console.log("im data2")
+            console.log(data)
+            productsModel.findOneAndUpdate({_id:data.id},{productName:data.productName,price:data.price,categoryId:data.categoryId},(err,results)=>{
                 if(err) reject(err);
                 resolve(results)
             })
--- routes/products-router.js
+++ routes/products-router.js
@@ -1,78 +1,98 @@
-const express =require("express");
-const router= express.Router();
+const express = require("express");
+const router = express.Router();
 
-const ProductsController= require("../controllers/products-controller")
+const ProductsController = require("../controllers/products-controller")
 
 
-router.get("/",async(req,res,next)=>{
-    try{
-        const result = await ProductsController.getProducts();
-        res.json(result);
-    }catch (ex){
-        res.status(404).send("eroor with get products")
-    }
+router.get("/", async (req, res, next) => {
+  try {
+    const result = await ProductsController.getProducts();
+    res.json(result);
+  } catch (ex) {
+    res.status(404).send("eroor with get products")
+  }
 })
 
-router.get("/:id",async(req,res,next)=>{
-    try{
-        const result = await ProductsController.getProductsById(req.params.id);
-        res.json(result);
-    }catch (ex){
-        res.status(404).send("eroor with get products")
-    }
+router.get("/:id", async (req, res, next) => {
+  try {
+    const result = await ProductsController.getProductsById(req.params.id);
+    res.json(result);
+  } catch (ex) {
+    res.status(404).send("eroor with get products")
+  }
 })
 
-router.get("/edit/:id",async(req,res,next)=>{
-    console.log("ASD")
-    console.log(req.params.id)
-    try{
-        const result = await ProductsController.getProductToEdit(req.params.id);
-        res.json(result);
-    }catch (ex){
-        res.status(404).send("eroor with get product")
-    }
+router.get("/edit/:id", async (req, res, next) => {
+  console.log("ASD")
+  console.log(req.params.id)
+  try {
+    const result = await ProductsController.getProductToEdit(req.params.id);
+    res.json(result);
+  } catch (ex) {
+    res.status(404).send("eroor with get product")
+  }
 })
 
 router.post("/update", async (req, res, next) => {
-    console.log("inside update ")
-    console.log(req.body)
-    // console.log(req.files.file)
-
-    
-//   let tags = req.body.tag.split("#")
-//   tags.splice(0,1)
-
-  let uploadedFile = req.files.file;
-
-
-
-  const results = await ProductsController.updateProduct(uploadedFile.name,req.body);
-  
-
-
-  uploadedFile.mv(`pics/${uploadedFile.name}`,err => {
-    if (err) {
-      res.json({
-        message: `${req.files.file.name} was not saved`,
-        err: JSON.stringify(err)
+  console.log("indide update")
+  console.log(req.body)
+  if(req.body.changeImage=="true"){
+    console.log("change image = true")
+    try{
+      let uploadedFile = req.files.file;
+      let result= await ProductsController.updateProductWithPic(uploadedFile.name, req.body);
+      uploadedFile.mv(`pics/${uploadedFile.name}`, err => {
+        if (err) {
+          res.json(err);
+        } else {
+          res.json( "successfuly saved");
+        }
       });
-    } else {
-      res.json({ message: `${req.files.file.name} saved` });
+
+    }catch(ex){
+      res.json({ ex });
     }
-  });
-    //    if (req.body.data.session) {
-           
-    //        try {
-    //            const result = await UsersController.logout(req.body.data.session);
-    //            res.json(result); 
-    //        } catch (ex) {
-    //            res.status(404).send("erorr with logingout")
-    //        }
-    //    }else{
-    //        res.send("erorr with logingout")
-    //    }
-   })
+
+  }else{
+    console.log("change image = false")
+    try {
+      let result= await ProductsController.updateProductWithoutPic(req.body);
+      res.json(result);
+    } catch (ex) {
+      res.status(404).send(ex)
+    }
+    
+    
+  }
+
+  // let uploadedFile = req.files.file;
+  // const results = await ProductsController.updateProduct(uploadedFile.name, req.body);
+  // uploadedFile.mv(`pics/${uploadedFile.name}`, err => {
+  //   if (err) {
+  //     res.json({
+  //       message: `${req.files.file.name} was not saved`,
+  //       err: JSON.stringify(err)
+  //     });
+  //   } else {
+  //     res.json({ message: `${req.files.file.name} saved` });
+  //   }
+  // });
 
 
 
-module.exports= router;
+  //    if (req.body.data.session) {
+
+  //        try {
+  //            const result = await UsersController.logout(req.body.data.session);
+  //            res.json(result); 
+  //        } catch (ex) {
+  //            res.status(404).send("erorr with logingout")
+  //        }
+  //    }else{
+  //        res.send("erorr with logingout")
+  //    }
+})
+
+
+
+module.exports = router;
